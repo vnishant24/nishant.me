53b8e083770910dd59d6cd2458c4951f6cc34e20
vnishant24 <84161849+vnishant24@users.noreply.github.com>
2021-08-09 14:49:19 -0700

    Add files via upload
    receipe app file

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import React from 'react';
 import './App.css';
-import Forecast from './components/forecast/forecast.js';
+import { useState, useEffect } from 'react';
+import Recipe from './Recipe';
 function App() {
- return (
-   <div className="App">
-     <header className="React Weather App">
-       <h1>React Weather App</h1>
-     </header>
-     <main>
-       <Forecast />
-     </main>
-     <footer>
-       Page created by NISHANT VERMA
-     </footer>
-   </div>
- );
+  const APP_ID = "f838d816";
+  const APP_KEY = "63057943ec749b0eddb5fb2d237a2662";
+
+
+const [recipes, setRecipes] = useState([]);
+const [search, setSearch] = useState('');
+const [query, setQuery] = useState('chicken');
+
+useEffect( ()=> {
+  getRecipes();
+}, [query]);
+
+const exampleReq = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+const getRecipes = async () => {
+  const response = await fetch(exampleReq);
+  const data = await response.json();
+  setRecipes(data.hits);
 }
 
+const updateSearch = e => {
+  setSearch(e.target.value);
+}
+const getSearch = e => {
+  e.preventDefault();
+  setQuery(search);
+  setSearch('');
+
+
+}
+return (
+    <div className="App">
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" types="text" value={search} onChange ={updateSearch}/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+        <Recipe
+        mainData = {recipe}
+        key={recipe.recipe.label}
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+        />
+        ))}
+      </div>
+    </div>
+  );
+}
 export default App;
